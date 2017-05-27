705cdaa71e674fd15c75210a74711149493206d0
Josh Seides <me@joshseides.com>
2017-05-26 23:38:36 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+import Expo, { Constants } from 'expo';
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+
+import Button from './components/Button';
+import Card from './components/Card';
+import List from './components/List';
+import Board from './components/Board';
+
+class App extends React.Component {
+  render() {
+    return (
+      <ScrollView style={styles.container}>
+        <Text>HSA Devello</Text>
+        <Board name="Hi guys"/>
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: Constants.statusBarHeight,
+    paddingBottom: 50
+  },
+});
+
+Expo.registerRootComponent(App);
